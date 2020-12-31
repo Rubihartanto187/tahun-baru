c58c95069f1e3809b18cd68a3033e8820f329005
Rubihartanto187 <74975296+Rubihartanto187@users.noreply.github.com>
2020-12-31 23:14:25 +0700

    coding tahun baru

---
+++ test_LCD.ino
@@ -0,0 +1,18 @@
+#include  <Wire.h>
+#include  <LiquidCrystal_I2C.h>
+
+LiquidCrystal_I2C lcd(0x27,16,2);  // set the LCD address to 0x27 for a 16 chars and 2 line display
+
+void setup()
+{
+  Serial.begin(9600);
+  lcd.init();                      // initialize the lcd 
+  lcd.backlight();
+  
+   lcd.print("hellow "); // Start Printing
+}
+
+void loop()
+{
+  
+}
