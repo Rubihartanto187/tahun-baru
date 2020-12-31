0b5df8245af7e838630a60de95fb11d3e86682b5
Rubihartanto187 <74975296+Rubihartanto187@users.noreply.github.com>
2021-01-01 00:54:47 +0700

    Update test_LCD.ino

--- test_LCD.ino
+++ test_LCD.ino
@@ -1,18 +1,36 @@
-#include  <Wire.h>
 #include  <LiquidCrystal_I2C.h>
-
-LiquidCrystal_I2C lcd(0x27,16,2);  // set the LCD address to 0x27 for a 16 chars and 2 line display
-
+#include <Servo.h>
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+int hitung = 10;
+const int tombol = 13;
 void setup()
 {
   Serial.begin(9600);
-  lcd.init();                      // initialize the lcd 
+  lcd.init();
   lcd.backlight();
-  
-   lcd.print("hellow "); // Start Printing
+ pinMode(tombol, INPUT_PULLUP);
+  lcd.setCursor(0, 0);
+  lcd.print("rubi hartanto");
+ delay(2000);
+ lcd.clear();
 }
 
 void loop()
 {
-  
+  if(digitalRead(tombol) == LOW)  {
+    hitung--;
+    delay(1000);
+    lcd.setCursor(8, 0);
+    lcd.print(hitung);
+  }
+  while (hitung == 0) {
+    int i;
+    lcd.setCursor(0, 0);
+    lcd.print("selamat tahun baru 2021");
+    for (i = 0 ; i < 16; i++)
+    {
+      lcd.scrollDisplayLeft();
+      delay(250);
+    }
+  }
 }
